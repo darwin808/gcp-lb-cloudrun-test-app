704b4ab92df5f0234c51c7d369d181ac43f0bc16
darwin.apolinario <darwin.apolinario@gmail.com>
2024-03-06 19:16:43 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello World Zesty 👋!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
